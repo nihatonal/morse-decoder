018b86924cc9bbbd3968cd7ff8dcee43ba892ad0
Server <server@server.com>
2020-10-04 00:39:10 +0300

    docs: src/index.js updated

--- src/index.js
+++ src/index.js
@@ -38,7 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    var wordList = expr.match(/.{1,10}/g)
+  var result = [];
+  var newCode;
+
+
+
+  for (var i = 0; i < wordList.length; i++) {
+    var x = wordList[i].replace(/00000000/g, "").replace(/10/g, ".").replace(/11/g, "-").replace(/000000/g, "").replace(/0000/g, "").replace(/00/g, "").replace("**********", " ");
+    result.push(x);
+    newCode = result.join(" ")
+  }
+
+  var messageConverted = [];
+
+  newCode.split("  ").map(function(word) {
+    word.split(" ").map(function(letter) {
+      messageConverted.push(MORSE_TABLE[letter]);
+    });
+    messageConverted.push(" ");
+  });
+
+return messageConverted.join("").trim("");
 }
 
 module.exports = {
